e3c1e8b66ae8395b69a842da89c06060cc32d6f1
Dirk Lemstra <dirk@lemstra.org>
2026-09-04 19:22:06 +0200

    Published Magick.NET 14.17.0

--- tests/Magick.NET.Tests/MagickNETTests/TheVersionProperty.cs
+++ tests/Magick.NET.Tests/MagickNETTests/TheVersionProperty.cs
@@ -49,7 +49,7 @@ public partial class MagickNETTests
         [Fact]
         public void ShouldContainTheCorrectVersion()
         {
-            Assert.Contains("14.16.0", MagickNET.Version);
+            Assert.Contains("14.17.0", MagickNET.Version);
         }
     }
 }
