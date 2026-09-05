298053457c16f413a9df6edaca668b12289bea2d
Dirk Lemstra <dirk@lemstra.org>
2026-09-05 14:15:06 +0200

    Published Magick.NET 14.17.1

--- publish/update.version.cmd
+++ publish/update.version.cmd
@@ -1,10 +1,10 @@
 @echo off
 
-powershell .\update.version.ps1 -library "Magick.NET" -version "14.17.0"
-powershell .\update.version.ps1 -library "Magick.NET.Core" -version "14.17.0"
+powershell .\update.version.ps1 -library "Magick.NET" -version "14.17.1"
+powershell .\update.version.ps1 -library "Magick.NET.Core" -version "14.17.1"
 powershell .\update.version.ps1 -library "Magick.NET.AvaloniaMediaImaging" -version "1.1.15"
-powershell .\update.version.ps1 -library "Magick.NET.SystemDrawing" -version "8.0.26"
-powershell .\update.version.ps1 -library "Magick.NET.SystemWindowsMedia" -version "8.0.26"
+powershell .\update.version.ps1 -library "Magick.NET.SystemDrawing" -version "8.0.27"
+powershell .\update.version.ps1 -library "Magick.NET.SystemWindowsMedia" -version "8.0.27"
 powershell .\update.version.ps1
 
 echo Also update the TheVersionProperty.ShouldContainTheCorrectVersion unit test
